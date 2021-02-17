0a04a843e43658d156d9668c6cfd29f7ad534abb
wanghao <wanghao@ninghao.net>
2021-02-17 15:25:53 +0800

    注册计划任务（Cron）

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @Cron(CronExpression.EVERY_WEEK)
+  handleCron() {
+    console.log('您好');
   }
 }
