72ca4b87bfcf9ce056709337bda2012a6b686870
wanghao <wanghao@ninghao.net>
2021-02-23 10:14:30 +0800

    安装、配置、注册队列

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: 'app',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
