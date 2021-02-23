0fda9404140a24a3ab1696f5f8f67828cd4306df
wanghao <wanghao@ninghao.net>
2021-02-23 10:34:02 +0800

    队列任务事件监听器（Event listeners）

--- src/app.processor.ts
+++ src/app.processor.ts
@@ -1,10 +1,26 @@
-import { Process, Processor } from '@nestjs/bull';
-import { Job } from 'bull';
+import {
+  OnQueueActive,
+  OnQueueCompleted,
+  Process,
+  Processor,
+} from '@nestjs/bull';
+import { DoneCallback, Job } from 'bull';
 
 @Processor('app')
 export class AppProcessor {
   @Process('resizeImage')
-  processResizeImage(job: Job) {
+  processResizeImage(job: Job, done: DoneCallback) {
     console.log(`👷🏽‍♂️ 任务处理：缩放图像文件 ${job.data.file}`);
+    done(null, '成功 ~~~');
+  }
+
+  @OnQueueActive()
+  onActive(job: Job) {
+    console.log(`🏗  任务活动：正在处理任务 ${job.name} (${job.id}) ...`);
+  }
+
+  @OnQueueCompleted()
+  onCompleted(job: Job, result: any) {
+    console.log(`🏝  任务完成：${job.name} (${job.id})，${result}`);
   }
 }
