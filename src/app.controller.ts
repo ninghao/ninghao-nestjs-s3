362bc0624ae2ec959c5f76d9c5248f860b2c0439
wanghao <wanghao@ninghao.net>
2021-02-23 10:21:36 +0800

    任务生产者（Producers）

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { InjectQueue } from '@nestjs/bull';
+import { Controller, Get, Post } from '@nestjs/common';
+import { Queue } from 'bull';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @InjectQueue('app') private appQueue: Queue,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('images')
+  async postImages() {
+    const job = await this.appQueue.add('resizeImage', {
+      file: Date.now(),
+    });
+
+    console.log(job);
   }
 }
